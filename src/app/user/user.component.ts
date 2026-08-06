0d985f4a61788cf80cd945d6fd020f623032df64
Calafia <osunasaul98@live.com>
2026-08-05 22:01:07 -0700

    String Interpolation - Property Binding - State

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
   standalone: true,
   imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
+  selectedUser = DUMMY_USERS[randomIndex]; //Now this property can be accesed to the template
 
+  // Even it's this looks like a method, because the "get" keywoard you must consider it as a property
+  get imagePath() {
+    return 'assets/users/' + this.selectedUser.avatar;
+  }
+
+  onSelectUser() {
+    const localRandomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser = DUMMY_USERS[localRandomIndex];
+  }
 }
