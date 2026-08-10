6bae95b50c33e6ddc73c7c9f756844a611c02a0f
Calafia <osunasaul98@live.com>
2026-08-09 20:46:37 -0700

    Two-Ways Binding using Directives (ngModel)

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,14 +1,18 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css',
 })
 export class NewTaskComponent {
   @Output() cancelTask = new EventEmitter();
+  taskTitle = '';
+  taskSummary = '';
+  taskDate = '';
 
   OnCancelTask() {
     this.cancelTask.emit();
