c35f5226991d4bdee23416f51530296af27b739a
Calafia <osunasaul98@live.com>
2026-08-09 21:13:31 -0700

    Submit form

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { NewTaskData } from '../task/task.model';
 
 @Component({
   selector: 'app-new-task',
@@ -10,11 +11,29 @@ import { FormsModule } from '@angular/forms';
 })
 export class NewTaskComponent {
   @Output() cancelTask = new EventEmitter();
+  @Output() add = new EventEmitter<NewTaskData>();
+
+  //Two Way Binding with Directives
   taskTitle = '';
   taskSummary = '';
   taskDate = '';
 
+  //Two Way Binding with Signal
+  // taskTitle = signal('');
+  // taskSummary = signal('');
+  // taskDate = signal('');
+
+  // Any of those didn't change the syntax in the template
+
   OnCancelTask() {
     this.cancelTask.emit();
+  }
+
+  OnSubmit() {
+    this.add.emit({
+      title: this.taskTitle,
+      summary: this.taskSummary,
+      date: this.taskDate,
+    });
   }
 }
